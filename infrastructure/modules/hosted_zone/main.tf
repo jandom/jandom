2945b24388184da9f3cc7e3adaf2e43986202230
Jan Domanski <jandom@gmail.com>
2019-06-22 00:06:45 +0100

    Sub-module the www_record

--- infrastructure/modules/hosted_zone/main.tf
+++ infrastructure/modules/hosted_zone/main.tf
@@ -3,17 +3,17 @@ resource "aws_route53_zone" "primary" {
   name = "${var.domain_name}"
 }
 
-module "www_record" {
-   source = "./www_record"
-   zone_id = "${aws_route53_zone.primary.zone_id}"
-   domain_name = "${var.domain_name}"
-   a_records = "${var.a_records}"
-}
-
 resource "aws_route53_record" "cname" {
   zone_id = "${aws_route53_zone.primary.zone_id}"
   name    = "*.${var.domain_name}"
   type    = "CNAME"
   ttl     = "300"
   records = ["${var.domain_name}"]
+}
+
+module "www_record" {
+   source = "./www_record"
+   zone_id = "${aws_route53_zone.primary.zone_id}"
+   domain_name = "${var.domain_name}"
+   a_records = "${var.a_records}"
 }
