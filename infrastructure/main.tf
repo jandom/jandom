7f136df3c723cc5b7b928e14e0ffb4564cb74b07
Jan Domanski <jandom@gmail.com>
2019-06-25 00:00:26 +0100

    Update backend to remote Dynamo + S3 bucket

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,3 +9,23 @@ module "hosted_zone" {
    domain_name = "${var.domain_name}"
    a_records = "${var.a_records}"
 }
+
+terraform {
+  backend "s3" { # will handle correctly multiple workspaces
+    bucket         = "jandomanski-homepage"
+    region         = "eu-west-1"
+    profile        = "jandom-personal"
+    key            = "terraform.tfstate"
+    dynamodb_table = "jandomanski-homepage-terraform" # to prevent race conditions, table needs to be created manually
+  }
+}
+
+
+data "terraform_remote_state" "network" {
+  backend = "s3" # bucket needs to be created manually
+  config {
+    bucket = "jandomanski-homepage"
+    key    = "terraform.tfstate"
+    region = "eu-west-1" # make sure terraform has permissions
+  }
+}
