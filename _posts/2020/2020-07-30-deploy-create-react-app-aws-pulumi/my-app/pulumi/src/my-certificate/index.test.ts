429a1119bdb853992a916245a810f829c03e6685
Jan Domanski <jandom@gmail.com>
2020-10-02 13:47:26 +0100

    Cleanups

--- _posts/2020/2020-07-30-deploy-create-react-app-aws-pulumi/my-app/pulumi/src/my-certificate/index.test.ts
+++ _posts/2020/2020-07-30-deploy-create-react-app-aws-pulumi/my-app/pulumi/src/my-certificate/index.test.ts
@@ -32,28 +32,6 @@ pulumi.runtime.setMocks({
             ...inputs,
           },
         };
-      // case "aws:ec2/securityGroup:SecurityGroup":
-      //     return {
-      //         id: "sg-12345678",
-      //         state: {
-      //             ...inputs,
-      //             arn: "arn:aws:ec2:us-west-2:123456789012:security-group/sg-12345678",
-      //             name: inputs.name || name + "-sg",
-      //         },
-      //     };
-      // case "aws:ec2/instance:Instance":
-      //     return {
-      //         id: "i-1234567890abcdef0",
-      //         state: {
-      //             ...inputs,
-      //             arn: "arn:aws:ec2:us-west-2:123456789012:instance/i-1234567890abcdef0",
-      //             instanceState: "running",
-      //             primaryNetworkInterfaceId: "eni-12345678",
-      //             privateDns: "ip-10-0-1-17.ec2.internal",
-      //             publicDns: "ec2-203-0-113-12.compute-1.amazonaws.com",
-      //             publicIp: "203.0.113.12",
-      //         },
-      //     };
       default:
         return {
           id: inputs.name + "_id",
